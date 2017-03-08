d86bf5f48c62d16b11a8db9452bb10d2e5f43bac
Francisco Javier Barrios Gomez <fbarrios80@MacBook-Pro-de-Francisco.local>
2017-03-08 14:19:49 -0300

    first commit

---
+++ app/src/main/java/com/fbarrios/dev/twitterlogin/MainActivity.java
@@ -0,0 +1,55 @@
+package com.fbarrios.dev.twitterlogin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                new AuthUI.IdpConfig.Builder(AuthUI.TWITTER_PROVIDER).build()))
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            showToast("result!!!");
+            return;
+        }
+
+        if (resultCode == RESULT_CANCELED) {
+            showToast("canceled");
+            return;
+        }
+
+        if (resultCode == ErrorCodes.NO_NETWORK) {
+            showToast("no network");
+            return;
+        }
+    }
+
+    private void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
